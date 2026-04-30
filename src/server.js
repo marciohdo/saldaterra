ce41f944bf60a9dd028b32fc5eff87cf26aed9af
Marcio Heleno <marcioho@gmail.com>
2026-04-30 19:41:19 -0300

    auto: 2026-04-30 19:41:19

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('./load-env');
 const express = require('express');
-const { sendText, sendTyping } = require('./evolution-api');
+const { sendText, sendTyping, markAsRead } = require('./evolution-api');
 const { reply }             = require('./claude');
 const { inserirVisitante, buscarPGProximo } = require('./supabase');
 const conversation          = require('./conversation');
