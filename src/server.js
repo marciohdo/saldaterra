ad085d76908917ee6278ac9de23d2e3edc4f15ec
Marcio Heleno <marcioho@gmail.com>
2026-05-06 09:50:09 -0300

    auto: 2026-05-06 09:50:09

--- src/server.js
+++ src/server.js
@@ -23,10 +23,11 @@ const scheduler             = require('./scheduler');
 const app = express();
 app.use(express.json());
 
-const DADOS_RE      = /^#DADOS:(\{.*\})/;
-const CONVIDAR_RE   = /^#CONVIDAR:(\{.*\})/m;
-const PARTICIPOU_RE = /^#PARTICIPOU:(\{.*\})/m;
-const NAO_ATENDE_RE = /^#NAO_ATENDE:(\{.*\})/m;
+const DADOS_RE           = /^#DADOS:(\{.*\})/;
+const CONVIDAR_RE        = /^#CONVIDAR:(\{.*\})/m;
+const PARTICIPOU_RE      = /^#PARTICIPOU:(\{.*\})/m;
+const NAO_ATENDE_RE      = /^#NAO_ATENDE:(\{.*\})/m;
+const ESPERANDO_RE       = /^#ESPERANDO_RETORNO:(\{.*\})/m;
 const REMINDER_MS = 2 * 60 * 1000;
 
 const TEST_MODE                = process.env.TEST_MODE === 'true';
